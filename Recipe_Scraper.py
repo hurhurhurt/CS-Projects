5434d628705b5be88464c564036576000e8f7f28
Thomas Hur <thur1@binghamton.edu>
2019-12-24 20:34:05 -0500

    working parser

--- Recipe_Scraper.py
+++ Recipe_Scraper.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import pandas as pd
 from bs4 import BeautifulSoup
 
 def get_links(link):
@@ -16,26 +17,61 @@ def get_links(link):
         accum +=1
     return recipes
 
-def parse_recipe(link):
+def parse_recipe(link, df):
     soup = BeautifulSoup(requests.get(link).text, 'html.parser')
     data = json.loads(soup.select_one('script.yoast-schema-graph.yoast-schema-graph--main').text)
     #print(json.dumps(data, indent=4))  # <-- uncomment this to print all data
 
     recipe = next((g for g in data['@graph'] if g.get('@type', '') == 'Recipe'), None)
     if recipe:
-        print('Name          =', recipe['name'])
-        print('Prep Time     =', recipe['prepTime'])
-        print('Cook Time     =', recipe['cookTime'])
-        print('Total time    =', recipe['totalTime'])
-        print('Ingredients   =', recipe['recipeIngredient'])
-        print('Calories      =', recipe['nutrition']['calories'])
-        print('Review Count  =', recipe['aggregateRating']['ratingCount'])
-        print('Avg Rating    =', recipe['aggregateRating']['ratingValue'])
+        try:
+            name = recipe['name']
+        except KeyError:
+            name = link
+        try:
+            prep_time = recipe['prepTime']
+        except KeyError:
+            prep_time = 0
+        try:
+            cook_time = recipe['cookTime']
+        except KeyError:
+            cook_time = 0
+        try:
+            ingredients = recipe['recipeIngredient']
+        except KeyError:
+            ingredients = ""
+        try:
+            calories = recipe['nutrition']['calories']
+        except KeyError:
+            calories = -1
+        try:
+            review_count = recipe['aggregateRating']['ratingCount']
+        except KeyError:
+            review_count = -1
+        try:
+            average_rating = recipe['aggregateRating']['ratingValue']
+        except KeyError:
+            average_rating = -1
+        data = [[name, prep_time, cook_time, ingredients, calories, review_count, average_rating]]
+        temp_df = pd.DataFrame(data, columns=['Name', 'Prep Time', 'Cook Time', 'Ingredients',
+                                                     'Calories', 'Review Count', 'Avg Rating'])
+        result = pd.concat([df, temp_df], sort=True)
+        return result
+
+
+def create_csv():
+    df = pd.DataFrame(columns=['Name', 'Prep Time', 'Cook Time', 'Ingredients', 'Calories', 'Review Count', 'Avg Rating'])
+    return df
+
 
 def main():
     link = "https://thewoksoflife.com/category/recipes/chicken/page/"
     recipes = get_links(link)
-    print(recipes)
+    df = create_csv()
+    for i in recipes:
+        df = parse_recipe(i, df)
+    print(df.to_string())
+    df.to_csv('plswork.csv')
 
 if __name__ == "__main__":
     main()
