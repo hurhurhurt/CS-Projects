e9d6ef03a13bc96b2d072dd54b1f0849199c126e
Thomas Hur <thur1@binghamton.edu>
2019-12-25 23:45:20 -0500

    Updated approach

--- Recipe_Scraper.py
+++ Recipe_Scraper.py
@@ -3,30 +3,30 @@ import json
 import pandas as pd
 from bs4 import BeautifulSoup
 
-# iterates over link and returns a list of links to every recipe contained in each page
+
 def get_links(link):
+    ''' iterates over link and returns a list of links to every recipe contained in each page '''
     recipes = []
-    # recipe pages start with a /1/ and increment by 1 for each page, we will use an accumulator to iterate through each page
     accum = 1
-    while True: 
-        page = link + str(accum) + "/"        
+    while True:
+        page = link + str(accum) + "/"
         newPage = requests.get(page)
         if newPage.status_code == 404:
             break
         soup = BeautifulSoup(newPage.content, 'html.parser')
-        for i in soup.findAll("a", {"class":"entry-title-link"}):
-            recipes.append(i.get('href')) # adds every link found in the current page to the list
-        accum +=1
+        for page in soup.findAll("a", {"class": "entry-title-link"}):
+            recipes.append(page.get('href'))
+        accum += 1
     return recipes
 
-# creates a dataframe of parsed name, ingredients, calories, etc and appends it to existing dataframe
-def parse_recipe(link, df):
+
+def parse_recipe(link):
+    ''' Parses through a webpage and extracts relevant info, returns a dictionary. '''
     soup = BeautifulSoup(requests.get(link).text, 'html.parser')
     data = json.loads(soup.select_one('script.yoast-schema-graph.yoast-schema-graph--main').text)
-
     recipe = next((g for g in data['@graph'] if g.get('@type', '') == 'Recipe'), None)
     if recipe:
-        # we use try/except catch blocks because some recipes are missing several parameters: this prevents the program from stopping during execution
+        # we use try/except catch blocks because some recipes are missing several parameters
         try:
             name = recipe['name']
         except KeyError:
@@ -34,44 +34,63 @@ def parse_recipe(link, df):
         try:
             prep_time = recipe['prepTime']
         except KeyError:
-            prep_time = 0
+            prep_time = "NA"
         try:
             cook_time = recipe['cookTime']
         except KeyError:
-            cook_time = 0
+            cook_time = "NA"
+        try:
+            total_time = recipe['totalTime']
+        except KeyError:
+            total_time = "NA"
         try:
             ingredients = recipe['recipeIngredient']
         except KeyError:
-            ingredients = ""
+            ingredients = "NA"
         try:
             calories = recipe['nutrition']['calories']
         except KeyError:
-            calories = -1
+            calories = "NA"
         try:
             review_count = recipe['aggregateRating']['ratingCount']
         except KeyError:
-            review_count = -1
+            review_count = "NA"
         try:
             average_rating = recipe['aggregateRating']['ratingValue']
         except KeyError:
-            average_rating = -1
-        data = [[name, prep_time, cook_time, ingredients, calories, review_count, average_rating]]
-        temp_df = pd.DataFrame(data, columns=['Name', 'Prep Time', 'Cook Time', 'Ingredients',
-                                                     'Calories', 'Review Count', 'Avg Rating'])
-        result = pd.concat([df, temp_df], sort=True)
-        return result
+            average_rating = "NA"
+        # creates a dictionary that will be converted into DataFrame for performance purposes
+        dictionary = {'Name': name, 'Prep Time': prep_time, 'Cook Time': cook_time, 'Total Time': total_time,
+                      'Ingredients': ingredients, 'Calories': calories, 'Review Count': review_count,
+                      'Average Rating': average_rating}
+        return dictionary
 
-# creates empty dataframe where recipes will be appended
-def create_df():
-    df = pd.DataFrame(columns=['Name', 'Prep Time', 'Cook Time', 'Ingredients', 'Calories', 'Review Count', 'Avg Rating'])
-    return df
+def main():
+    dict_list = []
+    links = ['https://thewoksoflife.com/category/recipes/chicken/page/',
+             'https://thewoksoflife.com/category/recipes/beef-recipes/page/',
+             'https://thewoksoflife.com/category/recipes/fish-and-seafood/page/',
+             'https://thewoksoflife.com/category/recipes/pork/page/',
+             'https://thewoksoflife.com/category/recipes/lamb/page/',
+             'https://thewoksoflife.com/category/recipes/appetizers-and-snacks/page/',
+             'https://thewoksoflife.com/category/recipes/bread-and-pizza/page/',
+             'https://thewoksoflife.com/category/recipes/chinese-take-out/page/',
+             'https://thewoksoflife.com/category/recipes/dessert/page/',
+             'https://thewoksoflife.com/category/recipes/vegetables/page/',
+             'https://thewoksoflife.com/category/recipes/tofu/page/',
+             'https://thewoksoflife.com/category/recipes/soups-and-stocks/page/',
+             'https://thewoksoflife.com/category/recipes/vegetarian/page/',
+             'https://thewoksoflife.com/category/recipes/quick-and-easy/page/',
+             'https://thewoksoflife.com/category/recipes/rice-and-noodles-recipes/page/']
+    for link in links:
+        recipes = get_links(link)
+        for recipe in recipes:
+            dictionary = parse_recipe(recipe)
+            if dictionary:
+                dict_list.append(dictionary)
+    dict_list = pd.DataFrame(dict_list)
+    dict_list.to_csv("Recipes.csv")
 
 
 if __name__ == "__main__":
-    link = "https://thewoksoflife.com/category/recipes/chicken/page/"
-    recipes = get_links(link)
-    df = create_df()
-    for i in recipes:
-        df = parse_recipe(i, df)
-    print(df.to_string())
-    df.to_csv('plswork.csv')
+    main()
